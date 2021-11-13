d9c14f84622ea75bcb5b9dd849242e6993dbfbec
posadari <57573681+posadari@users.noreply.github.com>
2021-11-13 15:52:40 -0600

    add initial card

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,28 +1,31 @@
 import React from 'react'
 import { GoogleMap, LoadScript } from '@react-google-maps/api';
-
-const containerStyle = {
-    width: '400px',
-    height: '400px'
-  };
+import MediaCard from './components/Card'
   
   const center = {
     lat: -3.745,
     lng: -38.523
   };
 
+
 const Home = () => {
     return (
-        <LoadScript
-        googleMapsApiKey={process.env.GOOGLE_MAPS_API_KEY as string}
+      <div>
+        <div>
+          <LoadScript
+        googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY as string}
         >
         <GoogleMap
-          mapContainerStyle={containerStyle}
+          mapContainerStyle={{width: '100%', height: '100vh', position: 'absolute'}}
           center={center}
           zoom={10}>
           <></>
         </GoogleMap>
       </LoadScript>
+        </div>
+        
+      <MediaCard />
+      </div>
     )
 }
 
